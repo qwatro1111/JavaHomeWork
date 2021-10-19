aed44cd34c490aab9638c4951959a749f69e693b
qwatro1111 <mr.outstanding111@gmail.com>
2021-10-19 20:49:51 +0300

    save hw3

---
+++ src/com/pb/hw3/Array.java
@@ -0,0 +1,48 @@
+package com.pb.hw3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbersArr = new int[10];
+        int counter = 0;
+        while (counter < numbersArr.length) {
+            System.out.println("Enter any integer (entered " + (counter + 1) + " of " + numbersArr.length + "): ");
+            if (scanner.hasNextInt()) {
+                numbersArr[counter] = scanner.nextInt();
+                counter++;
+            } else {
+                scanner.next();
+                System.out.println("You entered a non-integer number!");
+            }
+        }
+
+        int arraySum = 0;
+        int positiveNumbers = 0;
+        for (int i = 0; i < numbersArr.length; i++) {
+            arraySum += numbersArr[i];
+            if (numbersArr[i] > 0) {
+                positiveNumbers++;
+            }
+        }
+
+        System.out.println("Your array: " + Arrays.toString(numbersArr));
+        System.out.println("arraySum: " + arraySum);
+        System.out.println("positiveNumbers: " + positiveNumbers);
+
+        for (int i = 0; i < numbersArr.length; i++) {
+            for (int j = 0; j < numbersArr.length - 1; j++) {
+                if (numbersArr[j] > numbersArr[j + 1]) {
+                    int number = numbersArr[j];
+                    numbersArr[j] = numbersArr[j + 1];
+                    numbersArr[j + 1] = number;
+                }
+            }
+        }
+
+        System.out.println("Your array: " + Arrays.toString(numbersArr));
+    }
+}
---
+++ src/com/pb/hw3/Bingo.java
@@ -0,0 +1,44 @@
+package com.pb.hw3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Bingo {
+    public static void main(String[] args) {
+        int min = 0;
+        int max = 100;
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int randNumber = random.nextInt(max - min + 1) + min;
+        int customNumber;
+        String command;
+
+        int counter = 0;
+        while (true) {
+            counter++;
+            System.out.println("Number of attempts to guess the number: " + counter);
+            System.out.println("Enter your number (to exit enter \"exit\"): ");
+            if (scanner.hasNextInt()) {
+                customNumber = scanner.nextInt();
+                if (customNumber > randNumber) {
+                    System.out.println("Your number is greater than expected!");
+                } else if (customNumber < randNumber) {
+                    System.out.println("Your number is less than expected!");
+                } else if (customNumber == randNumber) {
+                    System.out.println("Congratulations, you guessed the number!");
+                    break;
+                } else {
+                    System.out.println("Undefined result");
+                }
+            } else {
+                command = scanner.next();
+                if (command.equals("exit")) {
+                    System.out.println("Game over!");
+                    break;
+                } else {
+                    System.out.println("You have not entered an integer! Please enter a whole number!");
+                }
+            }
+        }
+    }
+}
